cfbd523463ed49e670a7302b35ce2186ed5b4faa
bengeh <benjamingeh1993@gmail.com>
2018-06-26 15:23:13 -0700

    added a longest word challenge

--- randomquestions.go
+++ randomquestions.go
@@ -3,7 +3,10 @@ import (
     "fmt"
     "bufio"
     "os"
-    "strconv"
+   // "strconv"
+   "regexp"
+   "log"
+   "strings"
 )
 
 //Factorial example for Golang
@@ -18,12 +21,47 @@ func FirstFactorial(num int) int {
             
 }
 
+//Longest word challenge
+func LongestWord(text string) string{
+    
+    reg, err := regexp.Compile("[^a-zA-Z]+")
+    
+    if err != nil{
+        log.Fatal(err)
+    }
+    
+    processedString := reg.ReplaceAllString(text, " ")
+    
+    words := strings.Fields(processedString)
+    
+    longestword := 0
+    answer := ""
+    
+    for _, element := range words{
+        if len(element) > longestword{
+            longestword = len(element)
+            answer = element
+        }
+    }
+    fmt.Print(longestword)
+    fmt.Print(answer)
+    
+    return answer
+}
+
+
 func main() {
     scanner := bufio.NewScanner(os.Stdin)
-    fmt.Print("Enter your number: ")
+    fmt.Print("Enter your text: ")
     scanner.Scan()
-    number := scanner.Text()
-    i, _ := strconv.Atoi(number)
-    Fact := FirstFactorial(i)
-    fmt.Print(Fact)
+    text := scanner.Text()
+    word := LongestWord(text)
+    fmt.Print(word)
+    // for the factorial example 
+    //fmt.Print("Enter your number: ")
+    //scanner.Scan()
+    //number := scanner.Text()
+    //i, _ := strconv.Atoi(number)
+    //Fact := FirstFactorial(i)
+    //fmt.Print(Fact)
 }
