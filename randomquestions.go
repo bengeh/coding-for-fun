e46cc7e5445cde9b4db474b6f809b72737fb994c
bengeh <benjamingeh1993@gmail.com>
2018-06-26 16:36:58 -0700

    added new functions and play around with map

--- randomquestions.go
+++ randomquestions.go
@@ -1,8 +1,8 @@
 package main
 import (
     "fmt"
-    "bufio"
-    "os"
+    //"bufio"
+    //"os"
    // "strconv"
    "regexp"
    "log"
@@ -49,19 +49,63 @@ func LongestWord(text string) string{
     return answer
 }
 
+//reverse word challenge
+func ReverseWord(text string) string{
+    
+    runes := []rune(text)
+    for i,j := 0, len(runes) - 1; i<j;i, j = i + 1, j - 1{
+        runes[i], runes[j] = runes[j], runes[i]
+    }
+    return string(runes)
+
+}
+
 
 func main() {
+
+    //for the reverse challenge
+    /*
     scanner := bufio.NewScanner(os.Stdin)
+    fmt.Print("Enter your text: ")
+    scanner.Scan()
+    text := scanner.Text()
+    word := ReverseWord(text)
+    fmt.Print(word)
+    */
+    
+    //understanding maps in Golang
+    //create a map for fun
+    var m = make(map[string]int)
+    m["route"] = 66
+    fmt.Print(m)
+    
+    //check if there's a key called 'route' then print it's value
+    a, ok := m["route"]
+    if ok{
+        fmt.Print(a)
+    }
+    
+    //printing out the map
+    for key, value := range m{
+        fmt.Print("key: ", key, "Value: ", value)
+    }
+
+    //for the longest word challenge
+    /*scanner := bufio.NewScanner(os.Stdin)
     fmt.Print("Enter your text: ")
     scanner.Scan()
     text := scanner.Text()
     word := LongestWord(text)
     fmt.Print(word)
+    */
+    
+    /*
     // for the factorial example 
-    //fmt.Print("Enter your number: ")
-    //scanner.Scan()
-    //number := scanner.Text()
-    //i, _ := strconv.Atoi(number)
-    //Fact := FirstFactorial(i)
-    //fmt.Print(Fact)
+    fmt.Print("Enter your number: ")
+    scanner.Scan()
+    number := scanner.Text()
+    i, _ := strconv.Atoi(number)
+    Fact := FirstFactorial(i)
+    fmt.Print(Fact)
+    */
 }
